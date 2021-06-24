740dc71570947001350f85e94341ffe21b63f6f0
karlodinikozic <58643902+karlodinikozic@users.noreply.github.com>
2021-06-24 19:32:02 +0200

    kao treba radit

--- backend/routes/chatRouter.mjs
+++ backend/routes/chatRouter.mjs
@@ -3,11 +3,14 @@ import { Router} from "express";
 import { checkAccess } from "../middleware/userValidations.mjs";
 import { saveMessage } from "../controllers/chatController.mjs";
 import { createThread } from "../controllers/chatController.mjs";
+import { getChatThread } from "../controllers/chatController.mjs";
+import { checkChatHeader } from "../middleware/messageValidations.mjs";
 
 
 
 export const router = Router();
 const chatUrl = (req,res,next)=>{req.newUrl = findServerUrl(req,`chat`,'auth/checkToken');next();}
 
+router.post("/get",chatUrl,checkAccess,checkChatHeader,getChatThread)
 router.post("/chat",chatUrl,checkAccess,createThread)
 router.patch("/chat",chatUrl,checkAccess,saveMessage)
